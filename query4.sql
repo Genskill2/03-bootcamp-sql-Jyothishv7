1c4d3f60de254d98db0e918e63debefbb2f5375a
Jyothishv7 <61043734+Jyothishv7@users.noreply.github.com>
2021-06-19 23:38:38 +0530

    Update query4.sql

--- query4.sql
+++ query4.sql
@@ -1,4 +1,1 @@
-SELECT subjects.name FROM subjects 
-INNER JOIN books_subjects ON subjects.id = books_subjects.subject 
-INNER JOIN books ON books_subjects.book = books.id 
-WHERE books.title = 'Atomic Habits';
+SELECT s.name from subjects s, books_subjects bj WHERE bj.book = 6 AND bj.subject = s.id;
